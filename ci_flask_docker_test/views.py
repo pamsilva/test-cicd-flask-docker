f3a04308e1fb30a7c3802e449779d70c0dbc1bd6
pedro <ams.pedro@gmail.com>
2018-09-18 12:34:47 +0100

    first commit. Initial flask app running.

---
+++ ci_flask_docker_test/views.py
@@ -0,0 +1,11 @@
+from ci_flask_docker_test import app
+
+
+@app.route('/', methods=['GET'])
+def root():
+    return '<h1>Root Hello World</h1>'
+
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    return '<h1>Hello World</h1>'
