356cc7346d7f4fa95c9e336672f56472f864ba69
pedro <ams.pedro@gmail.com>
2018-09-20 13:01:21 +0100

    Testing change.

--- ci_flask_docker_test/views.py
+++ ci_flask_docker_test/views.py
@@ -3,9 +3,9 @@ from ci_flask_docker_test import app
 
 @app.route('/', methods=['GET'])
 def root():
-    return '<h1>Root Hello World !</h1>'
+    return '<h1>Root Hello World !?</h1>'
 
 
 @app.route('/hello', methods=['GET'])
 def hello():
-    return '<h1>Hello World !</h1>'
+    return '<h1>Hello World !?</h1>'
